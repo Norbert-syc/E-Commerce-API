63a19093e776e13cd6f59a64b11ed6b1fdb4dbe5
Norbert Nabahire <norbertnabahire@gmail.com>
2026-02-06 16:33:10 +0200

    fix

--- src/Models/ProductSchema.ts
+++ src/Models/ProductSchema.ts
@@ -4,7 +4,11 @@ import {IProduct} from "./ProductModel";
 const productSchema = new Schema<IProduct>({
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  categoryId: { type: String, required: true },
+  categoryId: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
   description: { type: String },
   image: { type: String, required: true },
   quantity: { type: Number, required: true }
